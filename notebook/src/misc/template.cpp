26b8ebe78fe9ebe80ec791955f8f777b6c03b689
haltode <tallancon@gmail.com>
2017-12-03 14:49:03 +0100

    add debug macros + shorten template

--- notebook/src/misc/template.cpp
+++ notebook/src/misc/template.cpp
@@ -1,19 +1,11 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <tuple>
+#include <bits/stdc++.h>
 
-#include <cmath>
-#include <cstdio>
-
-#define FOR(i, n) for(lli i = 0; i < (lli) n; ++i)
+#define FOR(i, n) for(lli i = 0; i < (lli)(n); ++i)
 #define ALL(x) (x).begin(), (x).end()
 
 #define pb push_back
 #define mt make_tuple
 #define mp make_pair
-
 #define fi first
 #define se second
 
@@ -22,18 +14,24 @@
 #define Z(A) get<2>(A)
 #define W(A) get<3>(A)
 
+#define DEBUG(X) { cerr << #X << " = " << (X) << endl; }
+#define outSET(X) { cerr << "set " << #X << " = "; for (auto _:X) cerr << _ << ' '; cerr << endl; }
+#define outVEC(X) { cerr << "vector "<< #X << " = "; for (auto _:X) cerr << _ << ' '; cerr << endl; }
+#define outMAP(X) { cerr << "map " << #X << " = "; for (auto _:X) cerr <<'('<<_.fi<<' '<<_.se<<')'<<' '; cerr << endl; }
+#define debugfunc(f,x) printf("%s(%g) = %g\n",#f,(x),(f(x)));
+#define debugfunc2(f,x,y) printf("%s(%g,%g) = %g\n",#f,(x),(y),(f((x),(y))));
+
 using namespace std;
 
-using lli = long long int;
+using ll = long long int;
 
-using vi = vector<lli>;
+using vi = vector<ll>;
 using vvi = vector<vi>;
-
 using vb = vector<bool>;
 using vvb = vector<vb>;
 
-using ii = pair<lli, lli>;
-using iii = pair<ii, lli>;
+using pi = pair<ll, ll>;
+using ppi = pair<pi, ll>;
 
 int main(void)
 {
